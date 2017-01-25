0c194949d943f86248bc094fda8a1579440cca43
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2017-01-25 22:54:22 +0100

    add eslintrc file

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'commonjs': true,
+        'es6': true
+    },
+    'extends': 'eslint:recommended',
+    'rules': {
+        "no-console":0,
+        "indent": [2],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ]
+    },
+    'globals': {
+        'gladys': true,
+        'it': true,
+        '__base': true,
+        'beforeEach': true,
+        'describe': true,
+        'process': true,
+        '__api': true,
+        '__base': true,
+        '__dirname': true
+    }
+};
