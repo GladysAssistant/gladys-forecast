7dea14ea69483d40eeaa7ab84195d8b1b629982e
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2017-01-25 22:47:42 +0100

    weather.get & init instance with API key

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 module.exports = function(sails) {
+
+    var init = require('./lib/init.js');
+    var get = require('./lib/weather/weather.get.js');
+
+    gladys.on('ready', function(){
+        init().catch(sails.log.warn);
+    });
     
     return {
+        weather: {
+            get
+        }
     };
 };
